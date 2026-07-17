67e2db45db1a754c5eaa5eab2fede8a49bd5248a
pratik252004-proton <protonx252004@gmail.com>
2026-07-17 15:02:58 +0530

    Update code

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import Profile from "../assets/Profile.png"
+import Profile from "../assets/Pro.png"
 import ParticlesBackground from "../components/ParticlesBackground";
 
 export default function About(){
